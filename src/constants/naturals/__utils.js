be1a6d6fc4931f1a3daddad32537edbeb9bd882d
Nicolò Ribaudo <hello@nicr.dev>
2024-05-10 19:24:25 +0200

    Await more

--- src/constants/naturals/__utils.js
+++ src/constants/naturals/__utils.js
@@ -1,5 +1,5 @@
 // Due to math, these utilities can only be prepared asynchronously!
-await null;
+await new Promise((resolve) => setTimeout(resolve, 5));
 
 export function double(dec) {
   return dec.add(dec);
